e81394f9aa3939c43db5657bbc8857b97106ab95
Hamood Siddiqui <70904930+Kesehet@users.noreply.github.com>
2024-05-05 21:00:27 +0530

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -8,10 +8,11 @@ pip install ollama transformers pydub pyannote.audio pytube
 pip install --upgrade git+https://github.com/huggingface/transformers.git accelerate
 pip install flash-attn --no-build-isolation
 
+# Download the file and save it as init.sh
+curl -o ollama.sh https://raw.githubusercontent.com/Kesehet/vastapibkup/main/ollama.sh
+chmod +x ollama.sh
+./ollama.sh
 
-
-ollama serve &
-ollama run llama3 &
 
 
 python3 /root/run.py
