be2848104fa8d23f99eacc3081ba722c654dc872
Hamood Siddiqui <70904930+Kesehet@users.noreply.github.com>
2024-05-05 12:22:07 +0530

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -1,20 +1,21 @@
 env >> /etc/environment;
-apt install python3 -y
-apt install python3-pip -y
-apt install curl -y
-apt install ffmpeg -y
+apt install -y python3 python3-pip curl ffmpeg --no-install-recommends
+curl -o /root/run.py https://raw.githubusercontent.com/Kesehet/vastapibkup/main/run.py &
+
 
 curl https://ollama.ai/install.sh | sh
 pip install ollama transformers pydub pyannote.audio pytube
 pip install --upgrade git+https://github.com/huggingface/transformers.git accelerate
 pip install flash-attn --no-build-isolation
 
+
+
 ollama serve &
 ollama run llama3 &
 
 
-
-
-curl -o /root/run.py https://raw.githubusercontent.com/Kesehet/vastapibkup/main/run.py
-
 python3 /root/run.py
+
+
+
+
