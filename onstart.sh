97c9267da99e037adfdfe99da44a20f39b8f59c7
Hamood Siddiqui <70904930+Kesehet@users.noreply.github.com>
2024-05-05 21:02:02 +0530

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -1,14 +1,13 @@
 env >> /etc/environment;
 apt install -y python3 python3-pip curl ffmpeg --no-install-recommends
 curl -o /root/run.py https://raw.githubusercontent.com/Kesehet/vastapibkup/main/run.py &
-
-
-curl https://ollama.ai/install.sh | sh
 pip install ollama transformers pydub pyannote.audio pytube
 pip install --upgrade git+https://github.com/huggingface/transformers.git accelerate
 pip install flash-attn --no-build-isolation
 
-# Download the file and save it as init.sh
+
+
+
 curl -o ollama.sh https://raw.githubusercontent.com/Kesehet/vastapibkup/main/ollama.sh
 chmod +x ollama.sh
 ./ollama.sh &
