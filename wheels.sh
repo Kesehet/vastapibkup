3f48b68100e62a26da68409cda932a8157abaf61
Hamood Siddiqui <70904930+Kesehet@users.noreply.github.com>
2024-06-05 06:32:43 +0530

    Update wheels.sh

--- wheels.sh
+++ wheels.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-apt-get install aria2
-mkdir wheels
+# Create a directory for the downloaded wheels
+mkdir -p wheels
+
 # Base URL for the wheel files
 BASE_URL="https://transpitch.com/python/wheel/"
 
-# Read the list of files from wheels.txt
+# Read the list of files from wheels.txt and download each using curl in the background
 while IFS= read -r file; do
-    # Download each file using aria2
-    aria2c -x 16 -s 16 -k 1M "${BASE_URL}${file}" -o "wheels/${file}" &
+    # Download each file using curl in the background
+    curl -o "wheels/${file}" "${BASE_URL}${file}" &
 done < wheels.txt
+
+# Wait for all background jobs to complete
+wait
