45d6639a1a3f2111e1374198298ccf2aed870a83
Hamood Siddiqui <70904930+Kesehet@users.noreply.github.com>
2024-06-05 06:27:15 +0530

    Update wheels.sh

--- wheels.sh
+++ wheels.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 apt-get install aria2
-
+mkdir wheels
 # Base URL for the wheel files
 BASE_URL="https://transpitch.com/python/wheel/"
 
 # Read the list of files from wheels.txt
 while IFS= read -r file; do
     # Download each file using aria2
-    aria2c -x 16 -s 16 -k 1M "${BASE_URL}${file}" -o "${file}" &
+    aria2c -x 16 -s 16 -k 1M "${BASE_URL}${file}" -o "wheels/${file}" &
 done < wheels.txt
