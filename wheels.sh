f41ea7bd7a8f8ceecbcacef723328653b784e4f5
Hamood Siddiqui <70904930+Kesehet@users.noreply.github.com>
2024-06-05 06:24:38 +0530

    Update wheels.sh

--- wheels.sh
+++ wheels.sh
@@ -8,5 +8,5 @@ BASE_URL="https://transpitch.com/python/wheel/"
 # Read the list of files from wheels.txt
 while IFS= read -r file; do
     # Download each file using aria2
-    aria2c -x 16 -s 16 -k 1M "${BASE_URL}${file}" -o "${file}"
+    aria2c -x 16 -s 16 -k 1M "${BASE_URL}${file}" -o "${file}" &
 done < wheels.txt
